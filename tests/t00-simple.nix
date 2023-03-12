303b4534ea94991faf95e9b6a3e28f5982b90e4e
Jörg Thalheim <joerg@thalheim.io>
2023-03-12 17:11:56 +0100

    add test for /serve feature

--- tests/t00-simple.nix
+++ tests/t00-simple.nix
@@ -1,5 +1,14 @@
 (import ./lib.nix)
-  ({ pkgs, ... }: {
+  ({ pkgs, ... }:
+  let
+    testServe = pkgs.runCommand "test" { } ''
+      mkdir -p $out/dir
+      echo file > $out/dir/file
+      ln -s /etc/passwd $out/forbidden-symlink
+      ln -s $out/dir/file $out/allowed-symlink
+    '';
+  in
+  {
     name = "t00-simple";
 
     nodes = {
@@ -10,7 +19,10 @@
           services.harmonia.enable = true;
 
           networking.firewall.allowedTCPPorts = [ 5000 ];
-          environment.systemPackages = [ pkgs.hello ];
+          system.extraDependencies = [
+            pkgs.hello
+            testServe
+          ];
         };
 
       client01 = { lib, ... }:
@@ -41,5 +53,18 @@
         assert data["version"] == 1, "version is not correct"
         assert data["root"]["entries"]["bin"]["type"] == "directory", "expect bin directory in listing"
         client01.succeed("${pkgs.hello}/bin/hello")
+
+        print("download ${testServe}")
+        out = client01.wait_until_succeeds("curl -v http://harmonia:5000/serve/${hashPart testServe}/")
+        print(out)
+        assert "dir/" in out, "dir/ not in listing"
+
+        out = client01.wait_until_succeeds("curl -v http://harmonia:5000/serve/${hashPart testServe}/dir")
+        print(out)
+        assert "file" in out, "file not in listing"
+
+        out = client01.wait_until_succeeds("curl -v http://harmonia:5000/serve/${hashPart testServe}/dir/file").strip()
+        print(out)
+        assert "file" == out, f"expected 'file', got '{out}'"
       '';
   })
